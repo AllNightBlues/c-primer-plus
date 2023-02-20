65f73b448a7e298545e145eb61a602611a1e93f4
AllNightBlues <stevezlh@hotmail.com>
2023-02-20 11:20:44 +0800

    add: exercise4-7 ~ exercise4-8

--- chap4/exercise4-7.c
+++ chap4/exercise4-7.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <float.h>
+
 
 
 int main(void) {
+
+
+    double double_1 = 1.0 / 3.0;
+    float float_1 = 1.0 / 3.0;
+
+    printf("%.6f, %.12f, %.16f\n", double_1, double_1, double_1);
+    printf("%.6f, %.12f, %.16f\n", float_1, float_1, float_1);
+    printf("FLT_DIG: %d\n", FLT_DIG);
+    printf("DBL_DIG: %d\n", DBL_DIG);
 
 
 
