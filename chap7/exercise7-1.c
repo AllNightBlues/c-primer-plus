518ba2ec0d991f78864469510a8cdc4e64a9686e
AllNightBlues <stevezlh@hotmail.com>
2023-03-22 17:38:26 +0800

    add: exercise7-1

---
+++ chap7/exercise7-1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void) {
+
+    char ch;
+    int count_of_space = 0;
+    int count_of_line_break = 0;
+    int count_of_other_char = 0;
+
+    printf("Please enter an input:");
+    ch = getchar();
+    while (ch != '#') {
+        if (ch == ' ') {
+            count_of_space++;
+        } else if (ch == '\n') {
+            count_of_line_break++;
+        } else {
+            count_of_other_char++;
+        }
+        ch = getchar();
+    }
+
+    printf("Count of Space: %i; Count of Line Break: %i; Count of Other Char: %i",
+           count_of_space, count_of_line_break, count_of_other_char);
+}
